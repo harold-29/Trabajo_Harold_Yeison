e10df27ba20ba06b7f1cdf27688710fa66a405ec
harold-29 <haroldbaduma@gmail.com>
2023-08-22 14:02:28 -0600

    V1

---
+++ src/PaqueteA/Principal.java
@@ -0,0 +1,17 @@
+
+package PaqueteA;
+
+/**
+ *
+ * @author Harold - Yeison
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        //Hola
+        
+        
+    }
+    
+}
